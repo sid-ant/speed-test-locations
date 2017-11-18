4051b774a9c8bb1db42bad446709c49500686e1f
Siddhant Chhabra <siddhantchhabra96@gmail.com>
2017-11-18 16:32:37 +0530

    Created Database

--- app.py
+++ app.py
@@ -1,5 +1,52 @@
-from flask import Flask,render_template
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'speed.db'),
+    SECRET_KEY='developmentkey',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+#app.config.from_envvar('SPEED_SETTINGS', silent=True)
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
 
 @app.route('/')
 def home():
---
+++ schema.sql
@@ -0,0 +1,17 @@
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  'date' varchar(240),
+  ipAddress varchar(240),
+  country varchar(240),
+  region varchar(240),
+  city varchar(240),
+  latitude real,
+  longitude real,
+  ispName varchar(240),
+  ispNameRaw varchar(240),
+  download real,
+  upload real,
+  latency real,
+  testId varchar(240)
+);
